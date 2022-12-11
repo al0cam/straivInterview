ffd091f0058f38783462ef00bdbc0695592877b5
Al0cam <benjaminsikac@gmail.com>
2022-12-11 17:21:57 +0100

    nokogiri improvements

--- tasks/1/XMLHandling.rb
+++ tasks/1/XMLHandling.rb
@@ -1,20 +1,28 @@
 require 'nokogiri'
 
-doc = Nokogiri.XML(File.open('xml_uncompressed.xml','r'))
-# doc = Nokogiri.XML(File.open('./tasks/1/helper.xml','r'))
+# doc = Nokogiri.XML(File.open('./tasks/1/xml_uncompressed.xml','r'))
+doc = Nokogiri.XML(File.open('./tasks/1/dummy.xml','r'))
 
 root = doc.root
 # puts root
 
 puts puts 
-puts root.at_xpath("soapenv:Body")
+# puts root.at_xpath("soapenv:Body")
 
-p root.at_xpath("//HotelReservationID[@ResID_Type='10']")
+# p root.xpath("//HotelReservationID[@ResID_Type='10']")
 
-p root.search("soapenv:Body")
+# p root.search("soapenv:Body")
 
-puts "ye"
+struct = OpenStruct.new
+struct.id = root.at_xpath("//HotelReservationID[@ResID_Type='10']").attr("ResID_Value")
+struct.external_id = root.at_xpath("//HotelReservationID[@ResID_Type='16']").attr("ResID_Value")
+struct.state = root.at_xpath("//HotelReservation").attr("ResStatus")
+struct.rate_plan = root.at_xpath("//RatePlanDescription").text.strip
+struct.services = root.xpath("//Services/Service").map{ |variable| variable.text.strip}.join(',')
 
-puts "cancer"
 
-# puts root.at_xpath("//Item[@filename='AGC_1164.jpg']")
+puts struct
+
+
+
+
