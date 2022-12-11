0519f509b19492ce409a934f9e92a9fab1bfa326
Al0cam <benjaminsikac@gmail.com>
2022-12-11 16:17:34 +0100

    fixed filepath error

--- tasks/4/base64ToImage.rb
+++ tasks/4/base64ToImage.rb
@@ -7,9 +7,10 @@ class Base64ToImage
         @file_data = File.open(path, 'r').read.gsub!("data:image/png;base64,","")
     end
 
-    def write_image_to_file(path)
-        FileUtils.mkdir_p(dirname) unless File.directory?(path)
-        File.open(path, 'wb') do |f|
+    def write_image_to_file(path,fileName)
+        FileUtils.mkdir_p(path) unless File.directory?(path)
+        puts path+fileName
+        File.open(path+fileName, 'wb') do |f|
             f.write(Base64.decode64(@file_data))
         end
     end
@@ -17,4 +18,4 @@ end
 
 reader = Base64ToImage.new
 reader.read_base64_from_file("./tasks/4/base64.txt")
-# reader.write_image_to_file("./tasks/4/rails/tmp/image.png")
+reader.write_image_to_file("./tasks/4/rails/tmp/","image.png")
